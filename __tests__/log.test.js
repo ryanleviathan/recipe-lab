6d693540321cccf699943df8f9cd84311be4ccb4
ryanleviathan <ryan.m.carreras@gmail.com>
2020-12-15 15:51:48 -0800

    passes 2nd test

--- __tests__/log.test.js
+++ __tests__/log.test.js
@@ -43,5 +43,33 @@ describe('log routes', () => {
     });
   });
 
+  it('finds all logs', async() => {
+    const recipe = await Recipe.insert({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
 
+    const log = await Log.insert({
+      dateOfEvent: new Date(),
+      notes: 'This is fantastic! Defintely make these again',
+      rating: 10,
+      recipeId: recipe.id
+    });
+
+    const res = await request(app)
+      .get('/api/v1/logs');
+
+    expect(res.body).toEqual([{
+      id: "1",
+      dateOfEvent: expect.anything(),
+      notes: 'This is fantastic! Defintely make these again',
+      rating: 10,
+      recipeId: recipe.id
+    }]);
+  });
 });
