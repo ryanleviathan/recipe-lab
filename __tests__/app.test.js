1a08dc1899424cb5e841ce5c66e3d439f63e826c
ryanleviathan <ryan.m.carreras@gmail.com>
2020-12-15 13:58:25 -0800

    4th test written and passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,6 +9,10 @@ describe('recipe-lab routes', () => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
 
+  afterAll(() => {
+    return pool.end();
+  });
+
   it('creates a recipe', () => {
     return request(app)
       .post('/api/v1/recipes')
@@ -85,5 +89,30 @@ describe('recipe-lab routes', () => {
           ]
         });
       });
+  });
+
+  it('gets a recipe by id', async() => {
+    const recipe = await Recipe.insert({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
+    const res = await request(app)
+      .get(`/api/v1/recipes/${recipe.id}`);        
+        
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ]
+    });
   });
 });
